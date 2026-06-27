088b15c32032b47630d931358c3c19aad7f1702d
Khaledibrahim7 <khaledramadanza@gmail.com>
2026-06-27 12:25:46 +0300

    CHANGE THE STYLE

--- FRONEND SOWAR/sowar-frontend/src/app/app.config.ts
+++ FRONEND SOWAR/sowar-frontend/src/app/app.config.ts
@@ -1,6 +1,8 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
+import { provideTranslateService } from '@ngx-translate/core';
+import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { routes } from './app.routes';
 import { authInterceptor } from './core/auth.interceptor';
@@ -10,6 +12,14 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([authInterceptor, errorInterceptor]))
+    provideHttpClient(withInterceptors([authInterceptor, errorInterceptor])),
+    provideTranslateService({
+      fallbackLang: 'ar',
+      lang: 'ar',
+      loader: provideTranslateHttpLoader({
+        prefix: './assets/i18n/',
+        suffix: '.json'
+      })
+    })
   ]
 };
